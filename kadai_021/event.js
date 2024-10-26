5cc3d178a6b0ccd0b9cae5caa87a81165fe36b31
hoshi-2024 <4tatsuya12-2@ezweb.ne.jp>
2024-10-27 04:51:40 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+        text.textContent = 'ボタンをクリックしました';
+    }, 2000);
+});
